80194cf63102932d29a56cf1a10245d2904022c4
Grant <gcermak@minthealth.io>
2018-02-07 19:54:36 -0600

    missing file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,12 +2,14 @@ var Set = artifacts.require("Set");
 var IterableSet_Integer = artifacts.require("IterableSet_Integer");
 var IterableSet_Address = artifacts.require("IterableSet_Address");
 var ShareCenter = artifacts.require("ShareCenter");
+var ImageShare = artifacts.require("ImageShare");
 module.exports = function(deployer) {
     deployer.deploy([Set, IterableSet_Address, IterableSet_Integer])
       .then(function () {
-      deployer.deploy(ShareCenter);
+      deployer.deploy([ShareCenter, ImageShare]);
     });
     deployer.link(Set, ShareCenter);
+    deployer.link(Set, ImageShare);
     deployer.link(IterableSet_Integer, ShareCenter);
     deployer.link(IterableSet_Address, ShareCenter);
 };
